f102e3fe6c1843f7232f6380233149680f8f551c
Yashraj Mungale <53795872+yashrajmungale@users.noreply.github.com>
2021-05-05 17:08:00 -0700

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://contosoqbot.azurewebsites.net/api/Request/",
+    selfUrl: "https://contosoqbot-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
+        tenantId: "cc88ed14-c42e-4fa9-bdd2-329627ebe066",
         clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
